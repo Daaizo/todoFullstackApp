e2feabfa29f8a68a4c66638faa437edb7fe9ccb9
Daaizo <danielcharlak@wp.pl>
2022-11-11 21:56:01 +0100

    s

---
+++ src/test/java/com/daaizo/springbootjpatest/SpringBootJpAtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daaizo.springbootjpatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpAtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
