b87130078f43bdd6aaffbaa4453ea1d2210dcb47
Daaizo <danielcharlak@wp.pl>
2022-11-13 20:30:07 +0100

    feat: error message in http requests added
    When an exception is thrown while executing http request. , an error message will appear in response

--- src/main/java/com/daaizo/todoFullstackApp/user/UserRepository.java
+++ src/main/java/com/daaizo/todoFullstackApp/user/UserRepository.java
@@ -1,10 +1,15 @@
 package com.daaizo.todoFullstackApp.user;
 
 import com.daaizo.todoFullstackApp.todo.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Long> {
-    List<Todo> findAllByTodos_name(String name);
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByLogin(String login);
 }
